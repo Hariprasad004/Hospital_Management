3988f2b4af7a499ee68919191cefb1bdf8434b24
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-11-15 16:03:02 +0530

    Alert

---
+++ Frontend/Js/alert.js
@@ -0,0 +1,12 @@
+//type should be success or error
+const showAlert = (type, msg) => {
+    hideAlert();
+    const markup = `<div class="alert alert--${type}">${msg}</div>`;
+    document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+    window.setTimeout(hideAlert, 4000);
+};
+
+const hideAlert = () => {
+    const el = document.querySelector('.alert');
+    if (el) el.parentElement.removeChild(el);
+}
