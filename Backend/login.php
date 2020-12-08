16a8b66f4de4ec68cccf2dc83dc64548c1be7495
mayuri3003 <mesthamayuri@gmail.com>
2020-12-08 19:47:09 +0530

    php code for login

---
+++ Backend/login.php
@@ -0,0 +1,40 @@
+<?php
+	if(isset($_POST['sbtbtn'])){
+	echo $emailid =$_POST['emailid'];
+	echo $pass=$_POST['pass'];
+
+	$con = mysqli_connect("localhost","root","","hospital");
+	//if($con->connect_error) {
+	//	die("Failed to connect : ".$con->connect_error);
+	//}
+	/*else{
+		$stmt =$con->prepare("select * from signup where emailid= ?");
+		$stmt->bind_param("s",$emailid);
+		$stmt->execute();
+		$stmt_result =$stmt->get_result();
+	}
+		if($stmt_result->num_rows > 0){
+			$data =$stmt_result->fetch_assoc();
+			if($data['pass']===$pass){
+				$error= "<h2>Login Successful</h2>";
+				header("location:htdocs/Hospital_management/home.html");
+			} else{
+				$error="<h2>Invalid email or password</h2>";
+			}
+
+		} else 
+			$error="<h2>Invalid username or password</h2>";
+}*/
+$query="SELECT * from signup where emailid='$emailid' and Password='$pass'";
+$obj=mysqli_num_rows(mysqli_query($con,$query));
+if($obj>0)
+{
+	//DASHBOARDPAGE
+	header('Location:home.html');
+}
+else
+{
+	echo "<h2>Invalid username or password <br> <a href='login.html'>CLICK HERE TO GO BACK</a></h2>";
+}
+}
+?>
--- Backend/signup.php
+++ Backend/signup.php
@@ -7,14 +7,14 @@
     echo $cpass=$_POST['cpass'];
 
 
-$conn=new mysqli('localhost','root','','hospital');
+$conn= new mysqli("localhost:3307","root","","hospital");
 if($conn->connect_error){
     die('connection failed :'.$conn->connect_error);
 }else{
-    $stmt = $conn->prepare("insert into signup(name,emailid,pass,cpass,) values(?,?,?,?)");
-    $stmt->bind_param("ssss",$name,$emailid,$pass,$cpass,);
+    $stmt = $conn->prepare("insert into signup(name,emailid,pass,cpass) values(?, ?, ?, ?)");
+    $stmt->bind_param("ssss",$name,$emailid,$pass,$cpass);
     $stmt->execute();
-    echo "signup successfully...";
+    echo "signup successful...";
     $stmt->close();
     $conn->close();
 }
