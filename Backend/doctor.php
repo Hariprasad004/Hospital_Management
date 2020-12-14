aebba50a411e2535e7e7248023adae7f2207d4e0
mayuri3003 <mesthamayuri@gmail.com>
2020-12-14 18:19:05 +0530

    admin_doctor

---
+++ Backend/doctor.php
@@ -0,0 +1,25 @@
+<?php
+//if(isset($_POST['sbtbtn']))
+//{
+    
+    echo $name=$_POST['name'];
+    echo $age=$_POST['age'];
+    echo $phoneno=$_POST['phoneno'];
+    echo $gender=$_POST['gender'];
+    echo $address=$_POST['address'];
+    echo $special=$_POST['special'];
+
+
+$conn= new mysqli("localhost:3307","root","","hospital");
+if($conn->connect_error){
+    die('connection failed :'.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into doctor(name,age,phoneno,gender,address,special) values(?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("siisss",$name,$age,$phoneno,$gender,$address,$special);
+    $stmt->execute();
+    echo "value passed";
+    $stmt->close();
+    $conn->close();
+}
+
+?>
