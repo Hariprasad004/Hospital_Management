d6f16693c9d36facefa2d2285085c73c84012f59
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-12-14 15:16:23 +0530

    Alert

--- Frontend/Php/alert.php
+++
@@ -1,9 +0,0 @@
-function showAlert($type, $msg){
-    $('.alert').hide();
-    $dom=DOMDocument.getElementById('body');
-    $elem = new DOMDocument('div', $msg);
-    $elem->setClass('alert alert--$msg');
-    $dom->appendChild($elem);
-    sleep(4000);
-    $html = preg_replace('#<div class="alert">(.*?)</div>#','',$html);
-}
